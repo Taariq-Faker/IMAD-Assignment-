ebae0cc0edc7c5bb1700421fbf6bb099c2139316
ST10479271 <ST10479271@vcconnect.edu.za>
2025-03-06 22:53:02 +0200

    UI Design stage

--- app/src/main/java/vcmsa/ci/taariqfaker10479271imadassignment1/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/taariqfaker10479271imadassignment1/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
@@ -15,28 +17,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val resetButton =findViewById<Button>(R.id.Button)
+        val resetButton = findViewById<Button>(R.id.resetButton)
 
-        val displaySugget=findViewById<TextView>(R.id.displaySugget)
+        val displaySugget = findViewById<TextView>(R.id.displaySugget)
 
-        val timeDayEdit=findViewById<EditText>(R.id.timeEditText)
+        val timeDayEdit = findViewById<EditText>(R.id.timeEditText)
 
-        Button?.setOnClickListener{
+        resetButton?.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Option Reset", Toast.LENGTH_LONG).show()
+
+            var time: Int
+            var meal: String
+            var morning: String
+            var MidMorning: String
+            var Afternoon: String
+            var AfternoonSnack: String
+            var Dinner: String
+            var AfterDinnerSnack: String
+
 
         }
-
-
-
-
-
-    }
-
-
-
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+}
