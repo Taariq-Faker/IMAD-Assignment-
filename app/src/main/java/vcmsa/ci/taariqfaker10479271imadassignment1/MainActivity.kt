aab961b8b8700f56d1e7cdc9b407b88f16c52cf3
ST10479271 <ST10479271@vcconnect.edu.za>
2025-03-06 22:25:31 +0200

    Beginning Attempt

---
+++ app/src/main/java/vcmsa/ci/taariqfaker10479271imadassignment1/MainActivity.kt
@@ -0,0 +1,42 @@
+package vcmsa.ci.taariqfaker10479271imadassignment1
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+        val resetButton =findViewById<Button>(R.id.Button)
+
+        val displaySugget=findViewById<TextView>(R.id.displaySugget)
+
+        val timeDayEdit=findViewById<EditText>(R.id.timeEditText)
+
+        Button?.setOnClickListener{
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Taariq Faker 10479271 IMAD Assignment 1"
+include(":app")
+ 
