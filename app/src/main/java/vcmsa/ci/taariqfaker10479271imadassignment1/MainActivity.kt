123acb04ce8570e22b9fc0877d0acf847552afac
ST10479271 <ST10479271@vcconnect.edu.za>
2025-03-07 23:04:05 +0200

    Working around with the if statements as well as setting up the reset button

--- app/src/main/java/vcmsa/ci/taariqfaker10479271imadassignment1/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/taariqfaker10479271imadassignment1/MainActivity.kt
@@ -19,24 +19,59 @@ class MainActivity : AppCompatActivity() {
 
         val resetButton = findViewById<Button>(R.id.resetButton)
 
-        val displaySugget = findViewById<TextView>(R.id.displaySugget)
+        val displaySuggest = findViewById<TextView>(R.id.displaySugget)
 
         val timeDayEdit = findViewById<EditText>(R.id.timeEditText)
+        val showOptions=findViewById<Button>(R.id.showOptions)
 
         resetButton?.setOnClickListener {
             Toast.makeText(this@MainActivity, "Option Reset", Toast.LENGTH_LONG).show()
 
-            var time: Int
-            var meal: String
-            var morning: String
-            var MidMorning: String
-            var Afternoon: String
-            var AfternoonSnack: String
-            var Dinner: String
-            var AfterDinnerSnack: String
+           timeDayEdit.setText("")
+            displaySuggest.setText("")
+        }
+
+        showOptions?.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Options generated", Toast.LENGTH_LONG).show()
+
+
+            if (timeDayEdit.text.toString()=="morning")
+            {
+                displaySuggest.setText("The recommended meal is Eggs")
+            }
+            else if (timeDayEdit.text.toString()=="Mid-morning Snack")
+            {
+                displaySuggest.setText("The recommended meal is a light snack,such as a fruit")
+            }
+            else if (timeDayEdit.text.toString()=="lunch")
+            {
+                displaySuggest.setText("The recommended meal is burger")
+
+            }
+            else if (timeDayEdit.text.toString()=="Afternoon Snack")
+            {
+                displaySuggest.setText("The recommended meal is a quick bite such as a cake or other sweet treats")
+            }
+            else if (timeDayEdit.text.toString()=="Main Course")
+            {
+                displaySuggest.setText("The recommended meal is is Pasta")
+            }
+            else if (timeDayEdit.text.toString()=="After Dinner Snack ")
+            {
+                displaySuggest.setText("The recommended meal is a dessert such as ice cream")
+            }
+
+
+
+
+
+
 
 
         }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
